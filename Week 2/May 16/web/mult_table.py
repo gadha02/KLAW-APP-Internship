7d536c732e7c48b50aea7831685df3020be69322
Gadha N S <120574593+gadha02@users.noreply.github.com>
2025-05-17 15:46:23 +0530

    Delete Week 2/May 16 directory

--- Week 2/May 16/web/mult_table.py
+++
@@ -1,9 +0,0 @@
-# filename: mult_table.py
-
-# Function to print multiplication table of a given number
-def print_multiplication_table(num, file=None):
-    for i in range(1, 11):
-        if file is not None:
-            print(f"{num} x {i} = {num * i}", file=file)
-        else:
-            print(f"{num} x {i} = {num * i}")
